df79919282846e5ab2611966ab57e408257d27ed
Mitch Ross <mitchross@dev-macbook-pro.local>
2018-10-07 17:38:55 -0400

    First app upload.

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Mitch Ross on 10/7/18.
+//  Copyright © 2018 vanillax. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        
+        
+        if !UIImagePickerController.isSourceTypeAvailable(.camera){
+            
+            let alertController = UIAlertController.init(title: nil, message: "Device has no camera.", preferredStyle: .alert)
+            
+            let okAction = UIAlertAction.init(title: "Alright", style: .default, handler: {(alert: UIAlertAction!) in
+            })
+            
+            alertController.addAction(okAction)
+            self.present(alertController, animated: true, completion: nil)
+            
+        }
+        else{
+            //other action
+            imagePicker.sourceType = .camera
+            imagePicker.allowsEditing = false
+        }
+        
+      
+        
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert uiimage to CIImage")
+            }
+            
+            detect(image: ciimage)
+            
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("loading coreML model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Error model failed to process image.")
+            }
+            
+            if let firstResult = results.first {
+                
+               self.navigationItem.title =  firstResult.identifier
+//                if firstResult.identifier.contains("keyboard") {
+//                    self.navigationItem.title = "Keyboard!"
+//                }
+//                else{
+//                    self.navigationItem.title = "Not Keyboard!"
+//
+//                }
+            }
+
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+        try handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
+        
+    }
+    
+
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+}
+
+
+
